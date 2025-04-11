5773699c132b10d3f449941b8a9466c381c32dd6
sarthak <sarthakkamble350@gmail.com>
2025-04-11 23:35:06 +0530

    axios.js updated

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:7000/api"
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:7000/api"
 const api = axios.create({
   baseURL: BASE_URL,
 });
